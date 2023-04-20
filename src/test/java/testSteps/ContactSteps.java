473fe848262e3fe362f3b63dc7ebf7cb4427259e
AsmaKM <129220171+AsmaKM@users.noreply.github.com>
2023-04-20 11:07:17 -0400

    deleted the contact feature

--- src/test/java/testSteps/ContactSteps.java
+++ src/test/java/testSteps/ContactSteps.java
@@ -1,7 +1,9 @@
 package testSteps;
 
 import io.cucumber.java.en.And;
+import org.openqa.selenium.WebDriver;
 import pages.Home;
+import pages.Ad;
 import pages.Contact;
 import properties.TestContext;
 
@@ -9,7 +11,10 @@ import static utils.AssertAndLog.assertTrueAndLog;
 
 public class ContactSteps {
     private Contact contact;
+    WebDriver driver;
+
     private Home home;
+    private Ad ad;
 
     public ContactSteps(TestContext textContext){
         contact = textContext.getPageManager().getContact();
@@ -17,8 +22,13 @@ public class ContactSteps {
 
 
     @And("I go to Contact Us Page")
-    public void iGoToContactUsPage() {
+    public void iGoToContactUsPage() throws InterruptedException {
     contact.goToContactUsPage();
+    if (this.ad==null){
+        driver.switchTo().defaultContent();
+    }
+    else         ad.closePopUpAd();
+
     }
 
     @And("I fill the form with name :{string}, email:{string}, subject: {string} and message: {string} and submit it")
--- src/test/resources/features/Contact.feature
+++
@@ -1,7 +0,0 @@
-Feature: Contact Form Test
-  Scenario: Fill Up the contact form and submit it
-    Given I go to Login page
-    When I try to login with valid credentials
-    Then I am logged in
-    And I go to Contact Us Page
-    And I fill the form with name :"asma", email:"asmaqac@gmail.com", subject: "testing" and message: "test" and submit it
