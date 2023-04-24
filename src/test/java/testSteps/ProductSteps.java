e35ee58f069576b1a2ef3f6455a96f48f0e71b66
AsmaKM <129220171+AsmaKM@users.noreply.github.com>
2023-04-24 10:26:27 -0400

    modification

--- src/test/java/testSteps/ProductSteps.java
+++ src/test/java/testSteps/ProductSteps.java
@@ -4,7 +4,6 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import pages.Home;
 import pages.Product;
-import pages.Ad;
 import properties.TestContext;
 import static utils.AssertAndLog.assertTrueAndLog;
 
@@ -13,10 +12,8 @@ public class ProductSteps {
 
     private Product product;
     private Home home;
-    private Ad ad;
     public ProductSteps(TestContext textContext){
         product = textContext.getPageManager().getProduct();
-        ad = textContext.getPageManager().getAd();
     }
 
     @And("I go to the Products Page")
@@ -37,7 +34,6 @@ public class ProductSteps {
     public void iCanSearchAndAddTwoToCart(String productName) throws InterruptedException {
         product.enterProductToSearch(productName);
         //Add 2 tshirts
-        ad.closePopUpAd();
         product.addTshirtsToCart();
     }
 
